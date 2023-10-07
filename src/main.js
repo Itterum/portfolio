581d42b2e3ec8554c4d828acf4d9278b5bc7e41f
itterum <itterum@proton.me>
2023-10-07 20:31:38 +0300

    add slider images

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-let count = 0;
-let countElement = document.getElementById("count");
+// Массив имен файлов изображений
+const imageNames = ['image1.png', 'image2.png', 'image3.png', 'image4.jpeg', 'image5.jpeg', 'image6.jpeg', 'image7.jpeg'];
 
-function increment() {
-  count++;
-  countElement.textContent = count;
-}
+// Получаем контейнер, куда будем добавлять изображения
+const imageListContainer = document.getElementById('image-list');
 
-function decrement() {
-  if (count > 0) {
-    count--;
-    countElement.textContent = count;
-  }
-}
+// Генерируем элементы DOM для каждой картинки
+imageNames.forEach((imageName, index) => {
+  const slide = document.createElement('div');
+  slide.classList.add('swiper-slide');
+
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('image-container');
+
+  const img = document.createElement('img');
+  img.src = 'src/images/' + imageName;
+  img.alt = 'Изображение ' + (index + 1);
+
+  imageContainer.appendChild(img);
+  slide.appendChild(imageContainer);
+  imageListContainer.appendChild(slide);
+});
+
+// Инициализируем Swiper после создания всех слайдов
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3, // Количество видимых слайдов
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});

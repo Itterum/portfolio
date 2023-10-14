e8a294e4d2a4cd880ae584593a3854351ec50905
itterum <itterum@proton.me>
2023-10-15 02:10:06 +0300

    simple card for img

---
+++ src/custom-button.ts
@@ -0,0 +1,21 @@
+import { html } from 'lit-html'
+
+export const customButton = html`
+<div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="memeowcats.jpeg">
+          <span class="card-title">Cat Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+`

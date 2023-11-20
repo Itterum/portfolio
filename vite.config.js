3c309f6359a48d2f5065ceac2f4b3356760bc5c9
Itterum <ivan.lyashenko.it@gmail.com>
2023-11-20 18:39:46 +0300

    fix assets import

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import solid from 'vite-plugin-solid'
 export default defineConfig({
   base: '/portfolio/',
   plugins: [solid()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
